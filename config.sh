512252a4de5b0544835ab1407ca62f7eb67149b7
Nicolas Hefti <nicolas@hefti.fr>
2015-04-29 22:54:01 +0200

    Initial commit

---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install pip
+if [ ! -d /usr/local/bin/pip ]
+	then echo "pip already installed"
+fi
+
+if [ ! -d /usr/local/bin/ansible ]
+	then echo "ansible already installed"
+fi
